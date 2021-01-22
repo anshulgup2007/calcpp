5c45c8ede5ee0aca672b0ea7804f4aaded4bf325
Anshul Kumar Gupta <76951617+anshulgup2007@users.noreply.github.com>
2021-01-22 10:05:41 +0530

    Add files via upload

---
+++ calculator1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+       int a, b, sum;
+       std::cout<<"Sum of two numbers:- " <<endl;
+       std::cout<<"Enter a and b values:- " <<endl;
+       cin>>a>>b;
+       sum = a + b;
+       cout<<sum<< endl;
+       return 0;
+}
